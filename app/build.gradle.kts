037d683d50c9f36598dd37fc8db20aa37f7607d2
suteny0r <suteny0r@gmail.com>
2026-08-09 18:28:55 -0400

    build: sign release APK with debug keystore for sideloading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
